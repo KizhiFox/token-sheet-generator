42abc487040e7efebf67ab4c2c6601597a3b222f
Артемчук Константин <atremchuk@gmail.com>
2021-08-16 15:07:17 +0300

    added tokens settings

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let tokensList = [];
 let pdfViewer = document.getElementById('pdf-viewer');
-pdfViewer.src = "placeholder.pdf"
+pdfViewer.src = "img/placeholder.pdf"
 
 function renderPDF() {
     let doc = new PDFDocument();
@@ -13,9 +13,64 @@ function renderPDF() {
     });
 }
 
+function getID(element) {
+    let splited = element.id.split('-');
+    let lastNumber = parseInt(splited[splited.length - 1]);
+    return lastNumber;
+}
+
 function showTokens() {
-    console.log('test');
-    console.log(tokensList);
+    let tokensHTML = document.getElementById('tokens-list');
+    tokensHTML.innerHTML='';
+    for (let i = 0; i < tokensList.length; i++) {
+        tokensHTML.innerHTML += `
+        <div class="token-card" id="token-${i}" index="${i}">
+          <table><tr>
+            <td>
+              <img class="card-img" src="${URL.createObjectURL(tokensList[i].img)}">
+            </td>
+            <td>
+              <input type="text" class="card-input" id="card-text-${i}" value="${tokensList[i].name}" onChange="changeName(this)"><br>
+              <label for="card-count-${i}" class="card-label" onChange="changeNumber(this)">Number of copies</label><br>
+              <input type="number" class="card-input" id="card-count-${i}" value="${tokensList[i].count}" onChange="changeNumber(this)"><br>
+              <label for="card-size-${i}" class="card-label">Size of token</label><br>
+              <select name="card-size-${i}" class="card-input" id="card-size-${i}" index="${i}" onChange="changeSize(this)">
+                <option value="tiny"${tokensList[i].size == 'tiny' ? ' selected' : ''}>Tiny</option>
+                <option value="small"${tokensList[i].size == 'small' ? ' selected' : ''}>Small</option>
+                <option value="medium"${tokensList[i].size == 'medium' ? ' selected' : ''}>Medium</option>
+                <option value="large"${tokensList[i].size == 'large' ? ' selected' : ''}>Large</option>
+                <option value="huge"${tokensList[i].size == 'huge' ? ' selected' : ''}>Huge</option>
+                <option value="gargantuan"${tokensList[i].size == 'gargantuan' ? ' selected' : ''}>Gargantuan</option>
+              </select>
+            </td>
+            <td>
+              <button id="card-delete-${i}" class="card-delete" id="card-delete-${i}"" onClick="deleteToken(this)">
+                <img class="card-delete-img" src="img/delete.png">
+              </button>
+            </td>
+            </tr></table>
+        </div>`;
+    }
+}
+
+function changeName(input) {
+    tokensList[getID(input)].name = input.value;
+    console.log(tokensList[getID(input)]);
+}
+
+function changeNumber(input) {
+    tokensList[getID(input)].count = parseInt(input.value);
+    console.log(tokensList[getID(input)]);
+}
+
+function changeSize(input) {
+    tokensList[getID(input)].size = input.value;
+    console.log(tokensList[getID(input)]);
+}
+
+function deleteToken(button) {
+    tokensList.splice(getID(button), 1);
+    showTokens();
 }
 
 function uploadTokens(input) {
